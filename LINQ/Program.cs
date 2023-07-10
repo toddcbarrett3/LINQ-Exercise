177d08c6d022e063140f805c1c7601c1dd881a64
Todd Barrett <toddcbarrett3@gmail.com>
2023-07-10 12:55:52 -0700

    Completed Exercise 2

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace LINQ
+﻿using System.Threading.Channels;
+
+namespace LINQ
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> gamesList = new List<string>();
+            gamesList.Add("The Legend of Zelda");
+            gamesList.Add("Donkey Kong");
+            gamesList.Add("Super Mario Bros.");
+            gamesList.Add("Tetris");
+            gamesList.Add("Excitebike");
+            gamesList.Add("Pac-Man");
+            gamesList.Add("Cuck Hunt");
+
+            gamesList.OrderBy(x => x.Length).ToList().ForEach(x => Console.WriteLine(x));
         }
     }
 }
